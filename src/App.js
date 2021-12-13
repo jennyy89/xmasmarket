7117ccbb3cf2840e54e4ee1bc5ce3e6824977d05
maauricez <maurice.raff@t-online.de>
2021-12-13 16:02:42 +0100

    mr

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
-
+import logo from "./White Christmas.png";
+import Snowfall from 'react-snowfall'
 function App() {
   return (
     <div className="App">
-      <div>
-        <img
-          className="background"
-          src="./xmasbackground"
-          alt="xmasbackground"
-        />
-      </div>
-      <div className="description">
-        <h1>White Christmas</h1>
-        <h3>Find the perfect Christmas Market for you</h3>
+  <div style={{ height: 0, width: 0, background: 'none' }}>
+    <Snowfall />
+  </div>
+
+  <div className="Container">
+  <div className="description">
+      <img src={logo} width="350" />
+        <h3 className="ue">Find the perfect Christmas Market for you.</h3>
       </div>
       <form action="/action_page.php">
-        <label for="places">Select Location</label>
+        <label for="places">Select Location:</label>
         <select id="place" name="place">
           <option value="munich">Munich</option>
           <option value="berlin">Berlin</option>
         </select>
         {/* <input type="submit" /> */}
       </form>
-      <button className="generatebtn">Generate</button>
+      <button className="generatebtn">Search</button>
+  </div>
     </div>
   );
 }
